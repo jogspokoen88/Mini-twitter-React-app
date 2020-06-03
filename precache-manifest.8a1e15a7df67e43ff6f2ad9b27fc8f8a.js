d7c79a22b574dec032ef49b63cf847326731be91
jogspokoen88 <jogspokoen88@mail.ru>
2020-06-03 23:10:31 +0500

    Updates

--- precache-manifest.8a1e15a7df67e43ff6f2ad9b27fc8f8a.js
+++ precache-manifest.8a1e15a7df67e43ff6f2ad9b27fc8f8a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b27ae1a7ffdfee64c0d9c55a4fa2398b",
+    "revision": "c2eb17dad5b4452753882164d019f441",
     "url": "/Mini-twitter-React-app.git/index.html"
   },
   {
-    "revision": "0b5d8f8187aa787e0f43",
+    "revision": "ee70dd934fcc722ec7cf",
     "url": "/Mini-twitter-React-app.git/static/css/main.556cc4d0.chunk.css"
   },
   {
-    "revision": "0c90a989334feb38c974",
-    "url": "/Mini-twitter-React-app.git/static/js/2.6ff71927.chunk.js"
+    "revision": "f4dd181a070fc9eb274b",
+    "url": "/Mini-twitter-React-app.git/static/js/2.ffd9ac25.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Mini-twitter-React-app.git/static/js/2.6ff71927.chunk.js.LICENSE.txt"
+    "url": "/Mini-twitter-React-app.git/static/js/2.ffd9ac25.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b5d8f8187aa787e0f43",
-    "url": "/Mini-twitter-React-app.git/static/js/main.4d6a774f.chunk.js"
+    "revision": "ee70dd934fcc722ec7cf",
+    "url": "/Mini-twitter-React-app.git/static/js/main.59c39d7b.chunk.js"
   },
   {
     "revision": "21272d1f6655f84fbcf8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mini-twitter-React-app.git/precache-manifest.9c4732141440f6dcb435260e20ac2abe.js"
+  "/Mini-twitter-React-app.git/precache-manifest.8a1e15a7df67e43ff6f2ad9b27fc8f8a.js"
 );
 
 self.addEventListener('message', (event) => {
